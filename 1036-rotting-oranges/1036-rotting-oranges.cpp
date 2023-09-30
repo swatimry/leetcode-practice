dadea0c826227e59ab99beb4a2d373a25de5eda1
Swati <115008404+swatimry@users.noreply.github.com>
2023-09-30 11:09:39 +0530

    Time: 14 ms (6.55%), Space: 13.5 MB (21.24%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -5,14 +5,12 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         
-        int vis[n][m];
+        
         queue<pair<int,pair<int,int>>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                   q.push({0,{i,j}});
-                }else{
-                    vis[i][j]=0;
                 }
                 if(grid[i][j]==1){
                     fresh++;
@@ -37,10 +35,10 @@ public:
                 int nrow=r+row[i];
                 int ncol=c+col[i];
                 cout<<nrow<<ncol;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m  && grid[nrow][ncol]==1){
                     q.push({t+1,{nrow,ncol}});
                     fresh--;
-                    vis[nrow][ncol]=2;
+                    grid[nrow][ncol]=2;
                     
 
 
