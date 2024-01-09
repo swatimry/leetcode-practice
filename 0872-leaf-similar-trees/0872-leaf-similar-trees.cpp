df06d6e5b5ee517503e87dc2b2cfd428a6a3529d
Swati <115008404+swatimry@users.noreply.github.com>
2024-01-09 08:13:23 +0530

    Time: 3 ms (66.12%), Space: 13.3 MB (49.18%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -27,17 +27,6 @@ public:
         vector<int> leaf2;
         findleaves(root1,leaf1);
         findleaves(root2,leaf2);
-        if(leaf1.size()==leaf2.size()){
-            for(int i=0;i<leaf1.size();i++){
-                if(leaf1[i]!=leaf2[i]){
-                    return false;
-                }
-            }
-            
-        }
-        else{
-            return false;
-        }
-        return true;
+        return leaf1==leaf2;
     }
 };
