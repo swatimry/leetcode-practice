39a6888ea3321a52a95d863f4515c9d59912c965
Swati <115008404+swatimry@users.noreply.github.com>
2024-02-18 11:05:23 +0530

    Time: 26 ms (83.07%) | Memory: 12 MB (90.60%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    int dp[10001][2^31];
-    int help(vector<int>& coins, int amount,int i){
-        if(amount==0){
-            return 0;
-        }
-        if(i<0 || amount<0){
-            return INT_MAX;
-        }
-        if(dp[amount][i]!=-1){
-            return dp[amount][i];
-        }
-       int take=help(coins,amount-coins[i],i);
-       int nottake=help(coins,amount,i-1);
-     if(take!=INT_MAX){
-         return  dp[amount][i]=min(1+take,nottake);
-     }
-     else{
-         return dp[amount][i]=nottake;
-     }
-
-    }
+   
     int coinChange(vector<int>& coins, int amount) {
-        int i=coins.size()-1;
-        memset(dp,-1,sizeof(dp));
-        int res= help(coins,amount,i);
-        return (res==INT_MAX) ? -1 :res;
+        
+        int dp[coins.size()+1][amount+1];
+         for (int i = 0; i <= coins.size(); i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    dp[i][j] = 0;
+                }
+                if (i == 0) {
+                    dp[i][j] = INT_MAX;  // Set to a large value instead of 0
+                }
+            }
+        }
+      
+        for(int i=1;i<coins.size()+1;i++){
+          for(int j=1;j<amount+1;j++){
+             
+              if(coins[i-1]<=j){
+                  int take=dp[i][j-coins[i-1]];
+
+                  int nottake=dp[i-1][j];
+                  if(take!=INT_MAX){
+                      dp[i][j]=min(1+take,nottake);
+                  }else{
+
+                 dp[i][j]=nottake;
+                  }
+               
+              }else{
+                   dp[i][j]=dp[i-1][j];
+              }
+          }
+        }
+        return dp[coins.size()][amount] == INT_MAX ? -1:dp[coins.size()][amount];
+       
+        
     }
 };
