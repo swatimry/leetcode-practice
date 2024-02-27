1b7df8fb936b5ff61ae1ff9284173f47b11a7c7f
Swati <115008404+swatimry@users.noreply.github.com>
2024-02-27 19:44:24 +0530

    Time: 7 ms (71.89%) | Memory: 9.9 MB (61.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,25 +4,27 @@ public:
        int n=word1.size();
        int m=word2.size();
        int dp[n+1][m+1];
-        for(int i=n;i>=0;i--){
-            dp[i][m]=word1.size()-i;
-        }
-         for(int j=m;j>=0;j--){
-            dp[n][j]=word2.size()-j;
-        }
-       for(int i=n-1;i>=0;i--){
-           for(int j=m-1;j>=0;j--){
+        for (int i = 0; i < n+1; i++)
+      dp[i][0] = i;
+
+    for (int j = 0; j <m+1; j++)
+      dp[0][j] = j;
+
+   
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=m;j++){
              
-                   if(word1[i]==word2[j]){
-                       dp[i][j]=dp[i+1][j+1];
+             
+                   if(word1[i-1]==word2[j-1]){
+                       dp[i][j]=dp[i-1][j-1];
                    }
                    else{
-                       dp[i][j]=1+min(dp[i+1][j+1]
-                       ,min(dp[i][j+1],dp[i+1][j]));
+                      
+          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
                    }
                
            }
        }  
-       return dp[0][0];
+       return dp[n][m];
     }
 };
