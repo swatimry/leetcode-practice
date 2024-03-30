172cae8c1537fb837c452b41d63d0ff10fa81be9
Swati <115008404+swatimry@users.noreply.github.com>
2024-03-30 20:07:12 +0530

    Time: 8 ms (9.95%) | Memory: 13.4 MB (77.74%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-      int s=0;int e=nums.size()-1;
-      int mid=s+(e-s)/2;
-      while(s<=e){
+int bs(int s,int e,int t,vector<int>&nums){
+    int mid=s+(e-s)/2;
+    while(s<=e){
         mid=s+(e-s)/2;
-           if(nums[mid]==target){
+        if(nums[mid]==t){
             return mid;
-           }
-           if (nums[mid]>=nums[s]){
-              if(target<=nums[mid] && target>=nums[s]){
-                e=mid-1;
-              }
-              else{
-                s=mid+1;
-              }
-           } 
-           else{
-              if(target>=nums[mid] && target<=nums[e]){
-                s=mid+1;
-              }
-              else{
-                e=mid-1;
-              }
-           }
-      }   
-      return -1;
+        }
+        if(nums[mid]>t){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        
+    }
+    return -1;
+}
+    int search(vector<int>& nums, int target) {
+        int k=-1;
+        int n=nums.size();
+        int s=0;int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        while(s<=e){
+         mid=s+(e-s)/2;
+         
+         int prev=(mid+n-1)%n;
+         int next=(mid+1)%n;
+         if(nums[prev]>=nums[mid] && nums[next]>=nums[mid]){
+            k= mid;
+            
+            break;
+         }
+         if(nums[mid]>=nums[e]){
+            s=mid+1;
+         }
+         else{
+            e=mid-1;
+         }
+
+        }
+        
+        int res=bs(0,k-1,target,nums);
+        if(res==-1){
+            return bs(k,n-1,target,nums);
+        }
+        return res;
     }
 };
