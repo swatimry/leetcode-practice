6ec334b0f9c3b1ed790a19f4607124694fbab364
Swati <115008404+swatimry@users.noreply.github.com>
2024-04-12 00:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (77.78%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -3,12 +3,23 @@ public:
     string removeDigit(string number, char digit) {
         int n=number.size();
         string ans="";
+        bool flag=false;
+        int idx=-1;
+        int lastidx=-1;
         for(int i=0;i<number.size();i++){
-
-            if(number[i]==digit){
-                string temp=number.substr(0,i)+number.substr(i+1,n-i);
-                ans=max(ans,temp);
+             if(number[i]==digit){
+                lastidx=i;
+             }
+            if(number[i]==digit && i+1<n && number[i]<number[i+1]){
+               
+               idx=i;
+               break;
             }
+        }
+        if(idx!=-1){
+            ans=number.substr(0,idx)+number.substr(idx+1,n-idx);
+        }else{
+            ans=number.substr(0,lastidx)+number.substr(lastidx+1,n-lastidx);
         }
         return ans;
     }
