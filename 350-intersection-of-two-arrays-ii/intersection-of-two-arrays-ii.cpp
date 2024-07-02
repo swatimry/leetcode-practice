d60b22c1a962ee8542f1210c9380d1b58df6622a
Swati <115008404+swatimry@users.noreply.github.com>
2024-07-02 23:03:38 +0530

    Time: 4 ms (59.42%) | Memory: 13 MB (50.21%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        map<int,int> m;
+        for(auto i:nums1){
+            m[i]++;
+        }
+        for(auto i:nums2){
+           if(m[i]>0){
+            ans.push_back(i);
+            m[i]--;
+           }
+        }
+       
+        return ans;
+    }
+};
