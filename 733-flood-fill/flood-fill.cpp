dc994720d82b8e682e03ff69a6d9359d4fc96c60
Swati <115008404+swatimry@users.noreply.github.com>
2024-07-05 16:39:02 +0530

    Time: 9 ms (28.36%) | Memory: 16.7 MB (65.92%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void help(int i,int j,int color,int src_color,int n,int m,vector<vector<int>>& image){
-      if(i<0 || j<0 ||i>n-1 || j>m-1 || image[i][j]!=src_color){
+void dfs(int sr,int sc,int color,int repcolor,vector<vector<int>>& image,int n,int m){
+    if(sr<0 ||sc<0|| sr>n-1 || sc>m-1 || image[sr][sc]!=repcolor || image[sr][sc]==color){
         return;
-      }
-      image[i][j]=color;
-      help(i,j+1,color,src_color,n,m,image);
-       help(i,j-1,color,src_color,n,m,image);
-        help(i-1,j,color,src_color,n,m,image);
-         help(i+1,j,color,src_color,n,m,image);
-
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        int src_color=image[sr][sc];
-        if(src_color!=color){
+    image[sr][sc]=color;
+    dfs(sr,sc+1,color,repcolor,image,n,m);
+dfs(sr,sc-1,color,repcolor,image,n,m);
+dfs(sr+1,sc,color,repcolor,image,n,m);
+dfs(sr-1,sc,color,repcolor,image,n,m);
 
-        help(sr,sc,color,src_color,n,m,image);
-        }
+
+
+}
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int repcolor=image[sr][sc];
+        int n=image.size();int m=image[0].size();
+        dfs(sr,sc,color,repcolor,image,n,m);
         return image;
     }
 };
