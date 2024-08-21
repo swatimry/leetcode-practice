039bd68b9ba8e271b3a86b501f2165964bf15ade
Swati <115008404+swatimry@users.noreply.github.com>
2024-08-21 13:05:45 +0530

    Time: 153 ms (5.04%) | Memory: 62.8 MB (6.97%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
- 
-   void dfs(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int u){
-       if(visited[u]==true) return;
-
-       visited[u]=true;
-       for(auto &v : adj[u]){
-           if(visited[v]==false){
-              dfs(adj,visited,v);
-           }
-       }
-   }
-
-
-    int makeConnected(int n, vector<vector<int>>& box) {
-        int cable_connection = box.size();
-        if(cable_connection<n-1){
+    void dfs(int curr,vector<bool> &visited, unordered_map<int,vector<int> > &adj){
+        visited[curr]=true;
+        for(auto i:adj[curr]){
+            if(!visited[i]){
+                dfs(i,visited,adj);
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1){
             return -1;
         }
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<box.size();i++){
-            int u = box[i][0];
-            int v = box[i][1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        unordered_map<int,vector<int> > adj;
+        for(int i=0;i<connections.size();i++){
+             adj[connections[i][0]].push_back(connections[i][1]);
+             adj[connections[i][1]].push_back(connections[i][0]);
         }
-   
-       int cnt = 0;
-       vector<bool> visited(n,false);
-       for(int i=0;i<n;i++){
-                if(visited[i]==false){
-                    cnt++;
-                    dfs(adj,visited,i);
-                }
-       }
-       return cnt-1;
+        vector<bool> visited(n,false);
+        int components=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                components++;
+               dfs(i,visited,adj);
+            }
+        }
+        
+        return components-1;
+
+
     }
 };
