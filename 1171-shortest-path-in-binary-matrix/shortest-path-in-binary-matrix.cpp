52ab38318c59af8cd67e50dbd37c383211dd9c9c
Swati <115008404+swatimry@users.noreply.github.com>
2024-08-24 19:59:59 +0530

    Time: 64 ms (30.52%) | Memory: 23.6 MB (76.59%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,39 @@
+typedef pair<int,pair<int,int>> pi;
 class Solution {
 public:
+  int dx[8]={0,1,-1,0,1,-1,-1,1};
+    int dy[8]={1,0,0,-1,1,-1,1,-1};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int count =0;
         int n=grid.size();
-        if(grid[0][0]==1){
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        vector<int> rows={-1,-1,0,+1,+1,+1,0,-1};
-         vector<int> cols={0,1,1,1,0,-1,-1,-1};
-        while(!q.empty()){
-               count++;
-            int nodes=q.size();
-            while(nodes--){
-           pair <int,int> temp=q.front(); q.pop();
-           
-           if(temp.first==n-1 && temp.second==n-1){
-            return count;
-           }
-           for(int i=0;i<8;i++){
-            int r=temp.first+rows[i];
-            int c=temp.second+cols[i];
-            if(r>=0 && c>=0 && r<n && c<n  && grid[r][c]!=1){
-               
-                q.push({r,c});
-                grid[r][c]=1;
-
-
+        vector<vector<int>> result(n,vector<int> (n,1e9));
+        result[0][0]=0;
+        priority_queue<pi,vector<pi>,greater<pi>> pq;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int x=pq.top().second.first;
+            int y=pq.top().second.second;
+            pq.pop();
+            for(int i=0;i<8;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]!=1){
+                    if(result[nx][ny]>1+dis){
+                        result[nx][ny]=1+dis;
+                        pq.push({result[nx][ny],{nx,ny}});
+                    }
+                }
             }
-           }
-            }
-          
-
 
         }
-        return -1;
+        if(result[n-1][n-1]==1e9){
+            return -1;
+        }
+        return result[n-1][n-1]+1;
+
+
     }
 };
