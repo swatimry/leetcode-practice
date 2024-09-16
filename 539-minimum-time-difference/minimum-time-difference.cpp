ee7c2fcdd169f77ede4e16844c63b7e3872bc1e7
Swati <115008404+swatimry@users.noreply.github.com>
2024-09-16 11:49:40 +0530

    Time: 20 ms (8.70%) | Memory: 18.4 MB (25.23%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -7,15 +7,16 @@ public:
    }
     int findMinDifference(vector<string>& timePoints) {
         sort(timePoints.begin(),timePoints.end());
-        timePoints.push_back(timePoints[0]);
+        
         int n=timePoints.size(); 
         int ans=INT_MAX;
         for(int i=0;i<n-1;i++){
            int currmin=convert(timePoints[i]);
            int nextmin=convert(timePoints[i+1]);
            ans=min(ans,abs(currmin-nextmin));
-           ans=min(ans,abs(currmin-(1440+nextmin)));
+          // ans=min(ans,abs(currmin-(1440+nextmin)));
         }
+        ans= min(ans,abs( convert(timePoints[n-1])-(1440+ convert(timePoints[0]))  ));
         return ans;
     }
 };
