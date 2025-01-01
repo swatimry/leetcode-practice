3faef053394396356ff40ca107be98d97874e1f6
Swati <115008404+swatimry@users.noreply.github.com>
2025-01-01 23:43:44 +0530

    Time: 3 ms (18.71%) | Memory: 9.5 MB (11.67%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(string s) {
+       vector<int> ones(s.size());
+       int n=s.size();
+       s[0]=='1'? ones[0]=1 : ones[0]=0;
+       for(int i=1;i<n;i++){
+         s[i]=='1'? ones[i]=ones[i-1]+1 : ones[i]=ones[i-1];
+       }
+       int tot_ones=ones[n-1];
+       int currzeroes=0;
+       int ans=INT_MIN;
+       for(int i=0;i<n-1;i++){
+        if(s[i]=='0'){
+            currzeroes++;
+        }
+         int temp= currzeroes+ tot_ones- ones[i];
+         ans=max(temp,ans);
+       }
+       return ans;
+    }
+};
