b413d9bdea0d0c15604b41903de3cc332a86e480
Swati <115008404+swatimry@users.noreply.github.com>
2025-03-02 12:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (99.95%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-   int dp[101][10001];
-    bool help(int i,int j,string s,string t){
-        if(i>=s.size()){
-            return true;
-        }
-        if(j>=t.size()){
-            return false;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return dp[i][j]= help(i+1,j+1,s,t);
-        }
-        return dp[i][j]=help(i,j+1,s,t);
-
-    }
     bool isSubsequence(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return help(0,0,s,t);
+      int i=0;int j=0;
+      while(i<s.size() && j<t.size()){
+        if(s[i]==t[j]){
+            i++;
+            
+        }
+        j++;
+      }
+      if(i==s.size()){
+        return true;
+      }   
+      return false;
     }
 };
